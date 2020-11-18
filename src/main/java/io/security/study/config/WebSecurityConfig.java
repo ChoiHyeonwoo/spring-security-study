6921332bd0d4fb5a1826dc548cf68f8b0dbd13fe
hyeonwoo <escape0619@gmail.com>
2020-11-18 21:30:20 +0900

    로그아웃 설정 커밋

--- src/main/java/io/security/study/config/WebSecurityConfig.java
+++ src/main/java/io/security/study/config/WebSecurityConfig.java
@@ -8,10 +8,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Configuration
@@ -45,6 +48,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     }
                 })
                 .permitAll()    //로그인 페이지는 접근가능하게끔
+        ;
+
+        http
+                .logout()
+                .logoutUrl("/logout") // 로그아웃처리
+                .logoutSuccessUrl("/login") //로그아웃 성공 url
+                .addLogoutHandler(new LogoutHandler() { // 추가 로그아웃 핸들러 (세션삭제, security context, 쿠키 삭제 등)
+                    @Override
+                    public void logout(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) {
+                        HttpSession session = httpServletRequest.getSession();
+                        session.invalidate();
+                    }
+                })
+                .logoutSuccessHandler(new LogoutSuccessHandler() {  // 로그아웃 성공 핸들러
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+                        httpServletResponse.sendRedirect("/login");
+                    }
+                })
+                .deleteCookies("remember-me")   //remember-me 쿠키 삭제
         ;
     }
 }
